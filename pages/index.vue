69ebd1528efaa5cb577603abe7ad28afb7821990
Aaron Barthel <aaron.barthel@live.ca>
2019-11-14 09:55:25 -0700

    Add netlify identity to index

--- pages/index.vue
+++ pages/index.vue
@@ -47,6 +47,14 @@
 //     Logo
 //   }
 // }
+
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
 
 <style>
